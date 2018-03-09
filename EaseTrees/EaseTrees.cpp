c90ecb73217dfa7986c5d7d182dc8f03d4f50e1e
ishu9bansal <ishubansal1400@gmail.com>
2018-03-09 14:17:36 +0530

    main file was empty

---
+++ EaseTrees/EaseTrees.cpp
@@ -0,0 +1,16 @@
+// EaseTrees.cpp : Defines the entry point for the console application.
+//
+
+#include "Header.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	testNode();
+	testSquareVector();
+	testMatrixGraph();
+	testNodeGraph();
+	return 0;
+}
