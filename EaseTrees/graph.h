b09ebb00c26e051e41a234a32ad1bfb3e1adb9c4
ishu9bansal <ishubansal1400@gmail.com>
2018-04-07 15:25:23 +0530

    remove vertex corrected
    remove vertex was not removing edges earlier,
    removed virtual keyword from non overriden methods (stlll dont know why it is used)

--- EaseTrees/graph.h
+++ EaseTrees/graph.h
@@ -60,6 +60,7 @@ public:
 	}
 	virtual void removeVertex(Index nodeIndex) {
 		// it replaces the last added obj at that place
+		removeEdgesOf(nodeIndex);
 		T lastNode = reverseMap.back();
 		dataMap[lastNode] = nodeIndex;
 		dataMap.erase(reverseMap[nodeIndex]);
@@ -68,32 +69,32 @@ public:
 		return;
 	}
 	virtual bool checkEdge(Index, Index, bool) = 0;	// pure virtual function
-	virtual bool checkEdge(Index a, Index b) {
+	bool checkEdge(Index a, Index b) {
 		return checkEdge(a, b, true) || checkEdge(a, b, false);
 	}
-	virtual bool checkEdge(const T& nodeA, const T& nodeB, bool AtoB) {
+	bool checkEdge(const T& nodeA, const T& nodeB, bool AtoB) {
 		return checkEdge(dataMap[nodeA], dataMap[nodeB], AtoB);
 	}
-	virtual bool checkEdge(const T& nodeA, const T& nodeB) {
+	bool checkEdge(const T& nodeA, const T& nodeB) {
 		return checkEdge( dataMap[nodeA], dataMap[nodeB]);
 	}
 
 	virtual void addEdge(Index, Index, bool) = 0;		// pure virtual function
-	virtual void addEdge(Index a, Index b) {
+	void addEdge(Index a, Index b) {
 		addEdge(a, b, true);
 		addEdge(a, b, false);
 	}
-	virtual void addEdge(const T& nodeA, const T& nodeB, bool AtoB) {
+	void addEdge(const T& nodeA, const T& nodeB, bool AtoB) {
 		addVertex(nodeA);
 		addVertex(nodeB);
 		addEdge(dataMap[nodeA], dataMap[nodeB], AtoB);
 	}
-	virtual void addEdge(const T& nodeA, const T& nodeB) {
+	void addEdge(const T& nodeA, const T& nodeB) {
 		addVertex(nodeA);
 		addVertex(nodeB);
 		addEdge(dataMap[nodeA], dataMap[nodeB]);
 	}
-	virtual void addVertex(const T& node, const vector<T>& nodeList, vector<bool> newToOld) {
+	void addVertex(const T& node, const vector<T>& nodeList, vector<bool> newToOld) {
 		addVertex(node);
 		Index nodeIndex = dataMap[node];
 		for (int i = 0; i < nodeList.size(); i++) {
@@ -101,7 +102,7 @@ public:
 			addEdge(nodeIndex, dataMap[nodeList[i]], newToOld[i]);
 		}
 	}
-	virtual void addVertex(const T& node, const vector<T>& nodeList, bool newToOld) {
+	void addVertex(const T& node, const vector<T>& nodeList, bool newToOld) {
 		addVertex(node);
 		Index nodeIndex = dataMap[node];
 		for (int i = 0; i < nodeList.size(); i++) {
@@ -109,7 +110,7 @@ public:
 			addEdge(nodeIndex, dataMap[nodeList[i]], newToOld);
 		}
 	}
-	virtual void addVertex(const T& node, const vector<T>& nodeList) {
+	void addVertex(const T& node, const vector<T>& nodeList) {
 		addVertex(node);
 		Index nodeIndex = dataMap[node];
 		int s = nodeList.size();
@@ -118,70 +119,70 @@ public:
 		return;
 	}
 	virtual void removeEdge(Index, Index, bool) = 0;		// pure virtual function
-	virtual void removeEdge(Index a, Index b) {
+	void removeEdge(Index a, Index b) {
 		removeEdge(a, b, true);
 		removeEdge(a, b, false);
 	}
-	virtual void removeEdge(const T& nodeA, const T& nodeB, bool AtoB) {
+	void removeEdge(const T& nodeA, const T& nodeB, bool AtoB) {
 		removeEdge(dataMap[nodeA], dataMap[nodeB], AtoB);
 	}
-	virtual void removeEdge(const T& nodeA, const T& nodeB) {
+	void removeEdge(const T& nodeA, const T& nodeB) {
 		removeEdge(dataMap[nodeA], dataMap[nodeB]);
 	}
-	virtual vector<Index> getChildren(Index x) {
+	vector<Index> getChildren(Index x) {
 		vector<Index> result;
 		for (int i = 0; i<reverseMap.size(); i++) {
 			if (checkEdge(x, Index(i), true))	result.push_back(i);
 		}
 		return result;
 	}
-	virtual vector<Index> getParents(Index x) {
+	vector<Index> getParents(Index x) {
 		vector<Index> result;
 		for (int i = 0; i<reverseMap.size(); i++) {
 			if (checkEdge(x, Index(i), false))	result.push_back(i);
 		}
 		return result;
 	}
-	virtual vector<Index> getAdjacent(Index x) {
+	vector<Index> getAdjacent(Index x) {
 		vector<Index> result;
 		for (int i = 0; i<reverseMap.size(); i++) {
 			if (checkEdge(x, Index(i)))	result.push_back(i);
 		}
 		return result;
 	}
-	virtual vector<Index> getChildren(const T& node) {
+	vector<Index> getChildren(const T& node) {
 		return getChildren(dataMap[node]);
 	}
-	virtual vector<Index> getParents(const T& node) {
+	vector<Index> getParents(const T& node) {
 		return getParents(dataMap[node]);
 	}
-	virtual vector<Index> getAdjacent(const T& node) {
+	vector<Index> getAdjacent(const T& node) {
 		return getAdjacent(dataMap[node]);
 	}
-	virtual void removeEdgesFrom(Index nodeIndex) {
+	void removeEdgesFrom(Index nodeIndex) {
 		vector<Index> adjacent = getChildren(nodeIndex);
 		for (int i = 0; i<adjacent.size(); i++)
 			removeEdge(nodeIndex, adjacent[i], true);
 	}
-	virtual void removeEdgesFrom(const T& node) {
+	void removeEdgesFrom(const T& node) {
 		removeEdgesFrom(dataMap[node]);
 	}
-	virtual void removeEdgesTo(Index nodeIndex) {
+	void removeEdgesTo(Index nodeIndex) {
 		vector<Index> adjacent = getParents(nodeIndex);
 		for (int i = 0; i<adjacent.size(); i++)
 			removeEdge(nodeIndex, adjacent[i], false);
 	}
-	virtual void removeEdgesTo(const T& node) {
+	void removeEdgesTo(const T& node) {
 		removeEdgesTo(dataMap[node]);
 	}
-	virtual void removeEdgesOf(Index nodeIndex) {
+	void removeEdgesOf(Index nodeIndex) {
 		removeEdgesFrom(nodeIndex);
 		removeEdgesTo(nodeIndex);
 	}
-	virtual void removeEdgesOf(const T& node) {
+	void removeEdgesOf(const T& node) {
 		return removeEdgesOf(dataMap[node]);
 	}
-	virtual void removeVertex(const T& node) {
+	void removeVertex(const T& node) {
 		removeVertex(dataMap[node]);
 	}
 
